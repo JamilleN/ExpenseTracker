eaa691e2751a164dbd593740b443a7c9e7d4dd47
Jamille Ni <jamilletni@gmail.com>
2023-07-22 15:48:19 -0400

    minor updates

--- src/App.js
+++ src/App.js
@@ -5,33 +5,33 @@ import { useState } from "react";
 const INITIAL_EXPENSES = [
   {
     id: "e1",
+    title: "Vet visit",
+    amount: 98.47,
+    date: new Date(2023, 6, 1),
+  },
+  {
+    id: "e2",
     title: "Replace Car Condenser",
     amount: 1921.99,
     date: new Date(2023, 4, 28),
   },
   {
-    id: "e2",
-    title: "New Couch",
-    amount: 1823.99,
-    date: new Date(2021, 10, 29),
+    id: "e3",
+    title: "Tuition",
+    amount: 7984.36,
+    date: new Date(2022, 8, 15),
   },
   {
-    id: "e3",
+    id: "e4",
     title: "Home Insurance",
     amount: 1553.04,
     date: new Date(2022, 6, 19),
   },
   {
-    id: "e4",
-    title: "Tuition",
-    amount: 7984.36,
-    date: new Date(2022, 8, 15),
-  },
-   {
     id: "e5",
-    title: "Vet visit",
-    amount: 98.47,
-    date: new Date(2023, 6, 1),
+    title: "New Couch",
+    amount: 1823.99,
+    date: new Date(2021, 10, 29),
   }
 ];
 
